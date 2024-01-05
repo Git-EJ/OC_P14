8db076b3f7719f5491733b2651beec814c819d3a
EJ <eric_j_fra@yahoo.fr>
2024-01-05 16:32:41 +0100

    homemade logo + header, add css reset + css base

---
+++ src/pages/EmployeeList.jsx
@@ -0,0 +1,7 @@
+const EmployeeList = () => {
+
+  return (
+  )
+}
+
+export default EmployeeList
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="header_container">
+        <div className="logo_container">
+          <img className="logo_img" src="/src/assets/logos/logo-circle_hr-net.png" alt="Logo HR Net" />
+          <h1 className="logo_title">HR net</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
