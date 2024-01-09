b8b92701929f61439a7f8d8038acbbe00b78af31
EJ <eric_j_fra@yahoo.fr>
2024-01-09 15:08:30 +0100

    spherePyramid components, css for header

--- src/molecules/Header.jsx
+++ src/molecules/Header.jsx
@@ -1,4 +1,5 @@
 import MultipleVerticalBar from "../atoms/MultipleVerticalBar";
+import SpherePyramid from "../atoms/SpherePyramid";
 
 
 const Header = () => {
@@ -6,17 +7,32 @@ const Header = () => {
   return (
     <>
       <header className="header_wrapper">
-        <div className="header_logo_and_vertical_bar_container">
+        <div className="header_contents_container">
+
           <div className="header_logo_container">
             <img className="header_logo_img" src="/src/assets/logos/logo-circle_hr-net.png" alt="Logo HR Net" />
             <h1 className="header_logo_title">HR net</h1>
           </div>
+
           <div className="header_vertical_bar_container">
-            <MultipleVerticalBar nbreOfBars={50} className={'header_vertical_bar_item'} />
+            <MultipleVerticalBar nbreOfBars={70} className={'header_vertical_bar_item'} />
+          </div>
+
+
+          <div className="header_button_container">
+            <div className="sphere-pyramid_wrapper">
+              <SpherePyramid />
+            </div>
+              <button className="header_button_nav">Employee list</button>
+            <div className="sphere-pyramid_wrapper">
+              <SpherePyramid />
+            </div>
           </div>
         </div>
+
         {/* empty div for the line under the logo and vertical bar */}
         <div className="header_border_bottom"></div> 
+        
       </header>
     </>
   );
