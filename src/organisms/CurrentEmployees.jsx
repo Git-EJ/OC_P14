b36469eb7cf59807da62040fe61d06fb312a7709
EJ <eric_j_fra@yahoo.fr>
2024-01-11 10:42:47 +0100

    data table add caret arrow for sort in title

--- src/organisms/CurrentEmployees.jsx
+++ src/organisms/CurrentEmployees.jsx
@@ -1,3 +1,8 @@
+import { useNavigate } from "react-router-dom";
+import SphereLine from "../atoms/SphereLine";
+import CaretUp from "../assets/icons/Caret_up";
+import CaretDown from "../assets/icons/Caret_down";
+
 const CurrentEmployees = () => {
 
   const arrayOfEmployeesDataTitle = [
@@ -14,39 +19,52 @@ const CurrentEmployees = () => {
 
   const arrayOfEmployeesDataContents = [
     {
-      firstName: "TOTO",
-      lastName: "TUTUTITI",
-      startDate: "01/01/2024",
-      department: "Human Ressources",
+      firstName: "ATOTO",
+      lastName: "ATUTUTITI",
+      startDate: "01/10/2024",
+      department: "AHuman Ressources",
       dateOfBirth: "02/02/2024",
-      street: "222 rue de TOTOVILLE",
-      city: "TOTOVILLE",
-      state: "TV",
-      zipCode: "00000",
+      street: "20 rue de TOTOVILLE",
+      city: "ATOTOVILLE",
+      state: "ATV",
+      zipCode: "01000",
     },
     {
-      firstName: "TOTO",
-      lastName: "TUTUTITI",
+      firstName: "BTOTO",
+      lastName: "BTUTUTITI",
       startDate: "01/01/2024",
-      department: "Human Ressources",
-      dateOfBirth: "02/02/2024",
-      street: "222 rue de TOTOVILLE",
-      city: "TOTOVILLE",
-      state: "TV",
-      zipCode: "00000",
+      department: "BHuman Ressources",
+      dateOfBirth: "10/02/2024",
+      street: "2 rue de TOTOVILLE",
+      city: "BTOTOVILLE",
+      state: "BTV",
+      zipCode: "00100",
     },
     {
-      firstName: "TOTO",
-      lastName: "TUTUTITI",
-      startDate: "01/01/2024",
-      department: "Human Ressources",
-      dateOfBirth: "02/02/2024",
-      street: "222 rue de TOTOVILLE",
-      city: "TOTOVILLE",
-      state: "TV",
-      zipCode: "00000",
+      firstName: "CTOTO",
+      lastName: "CTUTUTITI",
+      startDate: "10/01/2024",
+      department: "CHuman Ressources",
+      dateOfBirth: "02/01/2024",
+      street: "02 rue de TOTOVILLE",
+      city: "CTOTOVILLE",
+      state: "CTV",
+      zipCode: "00010",
+    },
+    {
+      firstName: "DTOTO",
+      lastName: "DTUTUTITI",
+      startDate: "10/01/2023",
+      department: "DHuman Ressources",
+      dateOfBirth: "02/01/2023",
+      street: "10 rue de TOTOVILLE",
+      city: "DTOTOVILLE",
+      state: "DTV",
+      zipCode: "00001",
     },
   ];
+
+  const navigate = useNavigate();
 
 
   const EmployeesDataTitle = () => {
@@ -54,6 +72,10 @@ const CurrentEmployees = () => {
       
       <div className={`current-employees_data-table_title_item_${index}`} key={`${index}_${title}`}>
         <p className= "current-employees_data-table_title_item_value">{title}</p>
+        <div className="current-employees_data-table_title_item_sorting_container">
+          <div className="current-employees_data-table_title_item_sorting_icon-up"><CaretUp /></div>
+          <div className="current-employees_data-table_title_item_sorting_icon-down"><CaretDown /></div>
+        </div>
       </div>
     ));
   };
@@ -74,6 +96,10 @@ const CurrentEmployees = () => {
     ));
   };
 
+  const addEmployeeLink = () => {
+    navigate('/employee-create');
+  }
+
   return (
     <main className="main_wrapper">
         <h2 className="current-employees_page-title">Employee List</h2>
@@ -82,10 +108,12 @@ const CurrentEmployees = () => {
 
           <div className="current-employees_data-table_options_container">
 
+            {/* TODO Tooltip */}
             <div className="current-employees_data-table_options_entries">
               <label htmlFor="data-table_entries">Show</label>
               <select id="data-table_entries">
-                <option value="10" selected>10</option>
+                <option value="1" selected>1</option>
+                <option value="10">10</option>
                 <option value="25">25</option>
                 <option value="50" >50</option>
                 <option value="100">100</option>
@@ -107,7 +135,29 @@ const CurrentEmployees = () => {
             <EmployeesDataContent />
           </div>
 
+          <div className="current-employees_data-table_showing_container">
 
+            <div className="current-employees_data-table_showing_entries_container">
+              <p className="current-employees_data-table_showing_entries_text">Showing 1 to 3 of 3 entries</p>
+            </div>
+
+            <div className="current-employees_data-table_showing_pagination_container">
+              <button className="current-employees_data-table_showing_pagination_button_previous">Previous</button>
+              <button className="current-employees_data-table_showing_pagination_button_current">1</button>
+              <button className="current-employees_data-table_showing_pagination_button_not-current">2</button>
+              <button className="current-employees_data-table_showing_pagination_button_next">Next</button>
+            </div>
+
+          </div>
+
+          {/* TODO delete employee with checkbox and button delete*/}
+          {/* TODO component for this button props type className onclick text value */}
+
+          <div className="form_submit_container">
+            <SphereLine />
+            <button type="submit" className="form_submit_button" onClick={addEmployeeLink} >Add Employee</button>
+            <SphereLine rotation={'east'} />
+          </div>
 
 
         </div>
