a9db99d117e61e50e491ce76dc27ac69b4596471
EJ <eric_j_fra@yahoo.fr>
2024-02-06 21:32:56 +0100

    Responsive spheres button

--- src/molecules/SpheresButton.jsx
+++ src/molecules/SpheresButton.jsx
@@ -1,24 +1,65 @@
 import PropTypes from "prop-types";
 import SphereLine from "../atoms/SphereLine";
 import useResponsiveRadius from "../atoms/style/useResponsiveRadius";
+import { useLayoutEffect, useState } from "react";
+import theme from "../theme";
 
 
 
 const SpheresButton = ({ type, className, onClick, text, container, maxRadius }) => {
 
+  const [innerRadius, setInnerRadius] = useState('68px'); // wheel logo height or width / 2 => find it in variable.scss
+  const [button, setButton] = useState({
+    size:{
+      width: '136px',
+      height: '136px'
+    },
+    fontSize: '1.3rem',
+    border: '10px',
+  });
+
+
+
+  useLayoutEffect(() => {
+    const updateButtonSize = () => {
+      const size = window.innerWidth > 600 ? { width: '135px', height: '135px' } : { width: '100px', height: '100px' };
+      const fontSize = window.innerWidth > 600 ? '1.3rem' : '1rem';
+      const border = window.innerWidth > 600 ? '6px': '4px';
+      const innerRadius = window.innerWidth > 600 ? '69px' : '51px';
+      setButton({size, fontSize, border});
+      setInnerRadius(innerRadius);
+
+    }
+    updateButtonSize();
+
+    window.addEventListener('resize', updateButtonSize);
+    return () => {
+      window.removeEventListener('resize', updateButtonSize);
+    }
+  }, []);
+
+  
   const responsiveRadius = useResponsiveRadius({container, maxRadius}) ;
+  
 
   return (
     // if you want to change the innerRadius, you have to change in spheresbutton.scss
     // the value of .spheres-button_container :nth-child(3) > :nth-child(1) 
     // first value of translate as egal for the innerRadius
     
-    // TODO logo responsive
-    // const container = window.innerWidth < 768 ? 'logosize' : 'logo size'
     <div className="spheres-button_container">
-      <SphereLine innerRadius="4.3rem" radius={responsiveRadius} angle={180} numberOfSpheres={4} gap="1px" />
-      <button type={type} className={className} onClick={onClick} >{text}</button>
-      <SphereLine innerRadius="4.3rem" radius={responsiveRadius} numberOfSpheres={4} gap="1px"/>
+      <SphereLine innerRadius={innerRadius} radius={responsiveRadius} angle={180} numberOfSpheres={4} gap="1px" />
+      
+      <button 
+        type={type} 
+        className={className} 
+        onClick={onClick} 
+        style={{fontSize: button.fontSize, ...button.size, border: `${button.border} solid ${theme.palette.main['primary-color']}`}}
+      >
+        {text}
+      </button>
+
+      <SphereLine innerRadius={innerRadius} radius={responsiveRadius} numberOfSpheres={4} gap="1px"/>
     </div>
   )
 };
